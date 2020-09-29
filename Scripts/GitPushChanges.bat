af7ad24cd7ed39ca34e3d9889fb98062d6c88287
Richard Knechtel <5502779+rknechtel@users.noreply.github.com>
2020-09-28 19:26:12 -0500

    Added more items

---
+++ Scripts/GitPushChanges.bat
@@ -0,0 +1,47 @@
+
+@echo off
+setlocal EnableDelayedExpansion
+REM ************************************************************************
+REM Script: GitPushChanges.bat
+REM Author: Richard Knechtel
+REM Date: 02/13/2020
+REM Description: This script will push code changes to Azure Repos (Git). 
+REM
+REM ************************************************************************
+
+echo.
+echo Running as user: %USERNAME%
+echo.
+
+REM Get parameters
+set GITCOMMITCOMMENT=%1
+
+echo.
+echo Parameters Passed: 
+echo Git Commit Comment: %GITCOMMITCOMMENT% 
+echo.
+
+REM Check if we got ALL parameters
+if "!GITCOMMITCOMMENT!"=="" goto usage
+
+REM Pushing Code Changes to Azure Repos (Git)
+@echo GitPushChanges.bat running
+@echo Pushing Code Changes to Azure Repos (Git)
+git add .
+git commit -m "%GITCOMMITCOMMENT%"
+git push origin master
+
+REM Lets get out of here!
+goto getoutofhere
+
+:usage
+set ERRORNUMBER=1
+echo [USAGE]: GitPushChanges.bat arg1
+echo arg1 = Git Commit Comment (Example: "Changed version in pom.") 
+goto getoutofhere
+
+REM ****************************************************************************
+REM Exit Script
+REM ****************************************************************************
+:getoutofhere
+Exit /B %ERRORNUMBER%
